a82e5c7f90c7d3d60625a23ed5c100372c9c93ec
toru-hihumi <robo_hihumi@outlook.jp>
2018-06-16 14:53:45 +0900

    first commit

---
+++ src/welcome.cpp
@@ -0,0 +1,22 @@
+/*
+ * welcome.cpp
+ *
+ *  Created on: 2018/06/15
+ *      Author: PCÅ[USER
+ */
+
+#include <iostream>
+
+int main() {
+	int a, b, c;
+	std::cin >> a;
+	std::cin >> b >> c;
+
+	std::string s;
+	std::cin >> s;
+
+	std::cout << a + b + c << " " << s << std::endl;
+
+	return 0;
+}
+
